befa76034d138b022e44fc712e39ed7d5eacace6
Your NameSamuel <prittirick144@gmail.com>
2024-12-02 03:00:06 -0500

    Update CORS configuration with proper headers

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://positivity-social-backend.onrender.com',
+    baseURL: 'https://positivity-social-backend.onrender.com/api',
     headers: {
         'Content-Type': 'application/json'
     }
@@ -10,21 +10,37 @@ const api = axios.create({
 // Add request interceptor
 api.interceptors.request.use(
     config => {
-        console.log('Request URL:', config.url);
+        // Add CORS headers to the request
+        config.headers = {
+            ...config.headers,
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+        };
         return config;
     },
-    error => {
-        return Promise.reject(error);
-    }
+    error => Promise.reject(error)
 );
 
 // Add response interceptor
 api.interceptors.response.use(
-    response => {
-        return response;
-    },
+    response => response,
     error => {
-        console.error('API Error:', error.message);
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.error('Response Error:', {
+                data: error.response.data,
+                status: error.response.status,
+                headers: error.response.headers
+            });
+        } else if (error.request) {
+            // The request was made but no response was received
+            console.error('Request Error:', error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.error('Error:', error.message);
+        }
         return Promise.reject(error);
     }
 );
